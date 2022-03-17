d5ee4d5e339cfa77c0a6f235b80c71426f2f46ab
xiaoke <yjmqaz@126.com>
2022-03-17 11:14:32 +0800

    Initial commit

---
+++ src/main/java/com/xk/imooc/Demo1Application.java
@@ -0,0 +1,13 @@
+package com.xk.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEsBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEsBlogApplication.class, args);
+    }
+
+}
